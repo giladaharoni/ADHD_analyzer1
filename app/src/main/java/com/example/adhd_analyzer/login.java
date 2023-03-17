a330cfcedfe63bf1f5c72c11db6d648151ac97a4
roni-git <roni.gilboa10@gmail.com>
2023-03-17 13:20:54 +0200

    login & register button don't in the good place

---
+++ app/src/main/java/com/example/adhd_analyzer/login.java
@@ -0,0 +1,44 @@
+package com.example.adhd_analyzer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class login extends AppCompatActivity {
+
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
+    private Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mUsernameEditText = findViewById(R.id.username_edit_text);
+        mPasswordEditText = findViewById(R.id.password_edit_text);
+        mLoginButton = findViewById(R.id.login_button);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUsernameEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
+                if (username.equals("admin") && password.equals("password")) {
+                    // Login success, go to home activity
+                    Intent intent = new Intent(login.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    // Login failed, show error message
+                    Toast.makeText(login.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
