3324e309b8fea81961d8a63f36f6ec6a799b976c
roni-git <roni.gilboa10@gmail.com>
2023-05-01 11:58:16 +0300

    menu!!!

--- app/src/main/java/com/example/adhd_analyzer/home.java
+++ app/src/main/java/com/example/adhd_analyzer/home.java
@@ -20,51 +20,8 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button menuButton = findViewById(R.id.menu_button);
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openOptionsMenu();
-            }
-        });
-        invalidateOptionsMenu();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.navigation_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.toolbar:
-                // Show the popup menu
-                View view = findViewById(R.id.toolbar);
-                PopupMenu popupMenu = new PopupMenu(this, view);
-                popupMenu.getMenuInflater().inflate(R.menu.navigation_menu, popupMenu.getMenu());
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        // Handle menu item click
-                        return true;
-                    }
-                });
-                popupMenu.show();
-                return true;
-            case R.id.item1:
-                Toast.makeText(this, "item 1 selected", Toast.LENGTH_SHORT);
-                return true;
-            case R.id.item2:
-                Toast.makeText(this, "item 2 selected", Toast.LENGTH_SHORT);
-                return true;
-            case R.id.item3:
-                Toast.makeText(this, "item 3 selected", Toast.LENGTH_SHORT);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
 
     }
+
+
 }
