0aebe74222f2cd9ddd65ef3bdf87de8b05041036
giladaharoni <giladah1@gmail.com>
2023-05-18 16:08:34 +0300

    integrate anychart

--- app/src/main/java/com/example/adhd_analyzer/ReportsFragment.java
+++ app/src/main/java/com/example/adhd_analyzer/ReportsFragment.java
@@ -16,10 +16,17 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.List;
 
 import static androidx.core.content.ContextCompat.getSystemService;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
 import com.example.adhd_analyzer.logger_sensors.ModuleDB;
+import com.example.adhd_analyzer.logger_sensors.ProcessedData;
+import com.itextpdf.awt.geom.Line2D;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,14 +35,6 @@ import com.example.adhd_analyzer.logger_sensors.ModuleDB;
  */
 public class ReportsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ReportsFragment() {
         // Required empty public constructor
@@ -53,8 +52,6 @@ public class ReportsFragment extends Fragment {
     public static ReportsFragment newInstance(String param1, String param2) {
         ReportsFragment fragment = new ReportsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -63,8 +60,8 @@ public class ReportsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
 
@@ -73,15 +70,24 @@ public class ReportsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_reports_fragment, container, false);
+        List<ProcessedData> data = ModuleDB.getProcessedDB(this.getContext()).processedDataDao().index();
+        View view;
+        if (data.isEmpty()){
+            view = inflater.inflate(R.layout.fragment_reports_fragment, container, false);
+        } else {
+            view = inflater.inflate(R.layout.fragment_report_unit,container,false);
+            Pie pie = AnyChart.pie();
+            float generalRatio = (float)data.stream().filter(ProcessedData::isHighAdhd).count() /
+                    (float)data.stream().filter(ProcessedData::isStayInPlace).count();
 
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_reports_fragment, container, false);
+
+
+
+        }
 
         // Find the download button by its ID and set an OnClickListener
         Button downloadButton = view.findViewById(R.id.download_button);
-        //ModuleDB.getProcessedDB(this.getContext()).processedDataDao().index();
+
 
 
         return view;
