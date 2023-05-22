13ad44b20a4c89fff9fbd12bd6f3992c18c6a002
giladaharoni <giladah1@gmail.com>
2023-05-22 11:15:24 +0300

    fix algorithm - add compression

--- app/src/main/python/data_process.py
+++ app/src/main/python/data_process.py
@@ -1,6 +1,21 @@
 import pandas as pd
 import numpy as np
 import json
+
+
+def compress(before_comp):
+    da = before_comp.copy()
+    da['timestamp'] = pd.to_datetime(da['timestamp'], unit='ms')
+    da.set_index('timestamp', inplace=True)
+    da_resampled = da.resample('5min').agg({'stay_inplace': 'sum', 'high_adhd': 'sum'})
+    da_resampled['high_adhd'] = da_resampled['high_adhd'] > da_resampled['high_adhd'].sum() / 2
+    da_resampled['stay_inplace'] = da_resampled['stay_inplace'] > da_resampled['stay_inplace'].sum() / 2
+    da_resampled.reset_index(inplace=True)
+    da_resampled['timestamp'] = (da_resampled['timestamp'] - np.datetime64('1970-01-01T00:00:00Z')) / np.timedelta64(1, 'ms')
+    da_resampled['timestamp'] = da_resampled['timestamp'].astype('int64')
+    return da_resampled
+
+
 def process(data):
     df = pd.DataFrame(json.loads(data))
     df = df.sort_values('timestamp')
@@ -16,4 +31,4 @@ def process(data):
     high_adhd = (df_diff_norm > major_threshold) & stay_inplace
     result_table = {'timestamp': timestamp, 'stay_inplace': stay_inplace,'high_adhd':high_adhd}
     result_table = pd.DataFrame(result_table)
-    return result_table
+    return compress(result_table)
