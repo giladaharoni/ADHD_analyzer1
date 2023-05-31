740dc7627ebb09e358522f0eccfd622b1048bf9c
giladaharoni <giladah1@gmail.com>
2023-05-31 13:00:56 +0300

    clear unsuccessfull graphs

--- app/src/main/java/com/example/adhd_analyzer/ReportsFragment.java
+++ app/src/main/java/com/example/adhd_analyzer/ReportsFragment.java
@@ -314,6 +314,7 @@ public class ReportsFragment extends Fragment {
             @Override
             public void onResponse(Call<List<DataGet>> call, Response<List<DataGet>> response) {
                 dataGetList = response.body();
+                charts.clear();
                 charts.add(lineChartGraph());
                 addChartToLayout(lineChartGraph(),graphContainer);
                 setTitle();
